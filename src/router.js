769d713eb6ca497220f011b7b4096480f1d94673
Brian.L <brian9500885@gmail.com>
2019-09-12 09:06:28 +0800

    add four page

--- src/router.js
+++ src/router.js
@@ -3,11 +3,17 @@ import Router from 'vue-router'
 import NotFound from './views/NotFound.vue'
 import SignIn from './views/SignIn.vue'
 import SignUp from './views/SignUp.vue'
+import Restaurants from './views/Restaurants.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'root',
+      redirect: '/restaurants'
+    },
     {
       path: '/signin',
       name: 'sign-in',
@@ -17,6 +23,26 @@ export default new Router({
       path: '/signup',
       name: 'sign-up',
       component: SignUp
+    },
+    {
+      path: '/restaurants',
+      name: 'restaurants',
+      component: Restaurants
+    },
+    {
+      path: '/restaurants/feeds',
+      name: 'restaurants-feeds',
+      component: () => import('./views/RestaurantsFeeds.vue')
+    },
+    {
+      path: '/restaurants/top',
+      name: 'restaurants-tops',
+      component: () => import('./views/RestaurantsTop.vue')
+    },
+    {
+      path: '/users/top',
+      name: 'users-top',
+      component: () => import('./views/UsersTop.vue')
     },
     {
       path: '*',
