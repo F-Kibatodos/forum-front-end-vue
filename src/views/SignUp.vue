f0ef1d4d8255f9fb9fd6a833e02db64af1a4d5bc
Brian.L <brian9500885@gmail.com>
2019-09-26 20:21:27 +0800

    api signup

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -72,6 +72,8 @@
 </template>
 
 <script>
+import authorizationAPI from "./../apis/authorization";
+import { Toast } from "./../utils/helpers";
 export default {
   name: "SignUp",
   data: function() {
@@ -83,16 +85,47 @@ export default {
     };
   },
   methods: {
-    handleSubmit(e) {
-      const data = JSON.stringify({
-        name: this.name,
-        email: this.email,
-        password: this.password,
-        passwordCheck: this.passwordCheck
-      });
-
-      // TODO: 向後端驗證使用者登入資訊是否合法
-      console.log("data", data);
+    async handleSubmit(e) {
+      try {
+        if (
+          !this.name ||
+          !this.email ||
+          !this.password ||
+          !this.passwordCheck
+        ) {
+          Toast.fire({
+            type: "warning",
+            title: "請確認已填寫所有欄位"
+          });
+          return;
+        }
+        if (this.password !== this.passwordCheck) {
+          Toast.fire({
+            type: "warning",
+            title: "兩次輸入的密碼不同"
+          });
+          this.passwordCheck = "";
+          return;
+        }
+        this.isProcessing = true;
+        const { data, statusText } = await authorizationAPI.signUp({
+          name: this.name,
+          email: this.email,
+          password: this.password,
+          passwordCheck: this.passwordCheck
+        });
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(data.message);
+        }
+        Toast.fire({
+          type: "success",
+          title: data.message
+        });
+        // 成功登入後轉址到登入頁
+        this.$router.push("/signin");
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 };
